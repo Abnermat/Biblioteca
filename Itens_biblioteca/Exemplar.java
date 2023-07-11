9e48ea0eeb17b4f654a5e5fe7bac6b97b52f04e4
Marcosmat <102193980+Marcosmat@users.noreply.github.com>
2023-07-11 19:47:03 -0300

    Update Exemplar.java

--- Itens_biblioteca/Exemplar.java
+++ Itens_biblioteca/Exemplar.java
@@ -1,48 +1,52 @@
 package trabalhoEngenharia.Itens_biblioteca;
 
-import java.util.*;
-
-import trabalhoEngenharia.Usuarios.Observer;
-
-
 public class Exemplar {
 //**********************************************
 	private static int contador = 1;
 	private String id;
 	private Livro livro;
-	private boolean emprestado;
+	private boolean disponivel;
+  	private Emprestimo emprestimo;
 	
-	public Exemplar(Livro livro) {
-		this.id = gerarId();
+	
+	public Exemplar(String id,Livro livro) {
+    		this.id = id;
+		this.setDisponivel(true);
 		this.livro = livro;
+		livro.addExemplar(this);
 	}
 
-    private String gerarId() {
-        String id = String.format("%02d", contador);
-        contador++;
-        return id;
-    }
+	private String gerarId() {
+		String id = String.format("%02d", contador);
+		contador++;
+		return id;
+	}
 
-    public String getId() {
-        return id;
-    }
-
+	public String getId() {
+		return id;
+	}
+	
 	public String getIdExemplar() {
 		return this.id;
 	}
-
-	public boolean isEmprestado() {
-		return this.emprestado; 
-	}
-
-
-	public void setEmprestado(boolean valor) {
-		this.emprestado = valor;
-	}
-
-	public Livro getLivro() {
-		return this.livro;
-	} 
 	
+	public boolean getDisponivel() {
+		return disponivel;
+	}
+	
+	
+	public void disponibilizar() {
+		disponivel = true;
+	}
+
+	public void emprestar(Emprestimo emprestimo){
+		this.emprestimo = emprestimo;
+		disponivel = false;
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+  
 		
 }
